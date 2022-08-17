8bd00e4608dd29278c9c714befdd5ecd88534fa7
barretoms <marcelo.barreto89@gmail.com>
2022-08-17 16:24:42 -0300

    modifications on classes and sql

---
+++ src/login/Authentication.java
@@ -0,0 +1,33 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Authentication {
+
+    private String username;
+    private String password;
+
+    public static void create(){
+
+    }
+
+    public static void validate(){
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        
+        // Connects to login database
+        String url = "";
+        String username = "";
+        String password = "";
+
+        Connection connection = null;
+        connection = DriverManager.getConnection(url, username, password);
+        return connection;
+    }
+
+
+}
